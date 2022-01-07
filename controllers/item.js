1b1e79c67ac3a6882631f0b396dd5013e0cdb512
shiyuc <shiyuc@students.cs.ubc.ca>
2022-01-07 14:15:34 -0800

    Readme added

---
+++ controllers/item.js
@@ -0,0 +1,20 @@
+const Item = require("../model/Item")
+
+const getAllItems = () => {
+}
+const getOneItem = () => {
+}
+const updateItem = () => {
+}
+const deleteItem = () => {
+}
+const createItem = () => {
+}
+
+module.exports = {
+    getAllItems,
+    getOneItem,
+    updateItem,
+    deleteItem,
+    createItem
+}
