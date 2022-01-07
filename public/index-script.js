ffdd3dcfacdccbda6fdedace5022a2f96e05a169
shiyuc <shiyuc@students.cs.ubc.ca>
2022-01-07 12:31:00 -0800

    index-script.js done

--- public/index-script.js
+++ public/index-script.js
@@ -11,18 +11,64 @@ const displayItems = async () => {
     loaderItems.style.visiblity = 'visible'
     try {
         const result = await axios.get("api/items")
+        const { items } = result
+
+        if (loaderItems.length < 1) {
+            itemsDiv.innerHTML = "<p>You Don't Have Any Item Yet</p>"
+            return
+        }
+
+        const itemSegments = items.map((item) => {
+            const { name, description } = item
+            return `<div class="ui segment">
+            <h5>${name}</h5>
+            <p>${description}</p>
+            <div class="item-buttons">
+            <!-- edit link -->
+            <a href="item.html?id=${itemID}"  class="edit-link">
+            <i class="fas fa-edit"></i>
+            </a>
+            <!-- delete btn -->
+            <button type="button" class="delete-btn" data-id="${itemID}">
+            <i class="fas fa-trash"></i>
+            </button>
+            </div>
+            </div>`
+        }).join("")
+
+        itemsDiv.innerHTML = itemSegments
+
     } catch (error) {
         alert(error)
+        itemsDiv.innerHTML = "<p>Error</p>"
     }
+    loaderItems.style.visiblity = 'hidden'
 
 }
 
+displayItems() // call to display all items at the start
+
+// Delete Task Button
+tasksDOM.addEventListener('click', async (e) => {
+    const el = e.target
+    if (el.parentElement.classList.contains('delete-btn')) {
+        loaderItems.style.visibility = 'visible'
+        const id = el.parentElement.dataset.id
+        try {
+            await axios.delete(`/api/items/${id}`)
+            displayItems()
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    loaderItems.style.visibility = 'hidden'
+})
 
 // Add Item Form
 itemForm.addEventListener("submit", async (e) => {
     e.preventDefault()
     const name = itemNameInput.value
-    const description = itemNameInput.description
+    const description = itemDescrition.value
 
     try {
         await axios.post("api/items", { name, description })
