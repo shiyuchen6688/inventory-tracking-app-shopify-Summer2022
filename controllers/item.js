c9da0230de899460d7afc59a9e902b2737ed8c5c
shiyuc <shiyuc@students.cs.ubc.ca>
2022-01-07 14:33:31 -0800

    item controllers done

--- controllers/item.js
+++ controllers/item.js
@@ -1,14 +1,35 @@
 const Item = require("../model/Item")
 
-const getAllItems = () => {
+const getAllItems = async (req, res) => {
+    const items = await Item.find({})
+    res.status(200).json({ items })
 }
-const getOneItem = () => {
+const getOneItem = async (req, res) => {
+    const item = await Item.find({ _id: req.params.id })
+    if (!item) {
+        // TODO: error handling
+    }
+    res.status(200).json({ item })
 }
-const updateItem = () => {
+const updateItem = async (req, res) => {
+    const item = await Item.findOneAndUpdate({ _id: req.params.id }, req.body)
+    if (!item) {
+        // TODO: error handling
+    }
+    res.status(200).json({ item })
 }
-const deleteItem = () => {
+const deleteItem = async (req, res) => {
+    const item = await Item.findOneAndDelete({ _id: req.params.id })
+    if (!item) {
+        // TODO: error handling
+    }
+    res.status(200).json({ item })
 }
-const createItem = () => {
+const createItem = async (req, res) => {
+    const item = await Item.create(req.body)
+    res.status(201).json({
+        item
+    })
 }
 
 module.exports = {
